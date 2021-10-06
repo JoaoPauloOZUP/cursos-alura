266a8388a1b76fdd8655d2205caa85883eca200d
Joao Paulo Oliveira <joao.lira@zup.com.br>
2021-10-06 17:54:13 -0300

    pízzaria testes

---
+++ dojo-pizzaria/src/test/java/br/com/zup/edu/pizzaria/ingredientes/cadastrodeingredientes/NovoIngredienteControllerTest.java
@@ -0,0 +1,74 @@
+package br.com.zup.edu.pizzaria.ingredientes.cadastrodeingredientes;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import javax.transaction.Transactional;
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+class NovoIngredienteControllerTest {
+
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void deveCadastrarNovoIngrediente() throws Exception {
+
+        NovoIngredienteRequest body = new NovoIngredienteRequest("Queijo muçarela", new BigDecimal("2.0"), 200);
+        MockHttpServletRequestBuilder request = post("/api/ingredientes")
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .content(new ObjectMapper().writeValueAsString(body));
+
+        mvc.perform(request)
+           .andExpect(status().isCreated())
+           .andExpect(header().exists("Location"))
+                .andExpect(redirectedUrlPattern("/api/ingredientes/*"));
+
+    }
+
+    @Test
+    void naoDeveCadastrarNovoIngredienteInvalido() throws Exception {
+
+        NovoIngredienteRequest body = new NovoIngredienteRequest("Queijo muçarela", new BigDecimal("2.0"), 0);
+        MockHttpServletRequestBuilder request = post("/api/ingredientes")
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .content(new ObjectMapper().writeValueAsString(body));
+
+        mvc.perform(request)
+           .andExpect(status().isBadRequest())
+                .andDo(print());
+
+    }
+
+    @Test
+    void naoDeveCadastrarIngredienteDuplicado() throws Exception {
+        NovoIngredienteRequest body = new NovoIngredienteRequest("Queijo muçarela", new BigDecimal("2.0"), 100);
+        MockHttpServletRequestBuilder request = post("/api/ingredientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(body));
+
+        mvc.perform(request)
+                .andExpect(status().isCreated())
+                .andDo(print());
+
+        mvc.perform(request)
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
+}
---
+++ dojo-pizzaria/src/test/java/br/com/zup/edu/pizzaria/pizzas/cadastropizza/NovaPizzaControllerTest.java
@@ -0,0 +1,57 @@
+package br.com.zup.edu.pizzaria.pizzas.cadastropizza;
+
+import br.com.zup.edu.pizzaria.ingredientes.cadastrodeingredientes.NovoIngredienteRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import javax.transaction.Transactional;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+class NovaPizzaControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void deveCadastrarNovaPizza() throws Exception {
+
+        NovoIngredienteRequest bodyIngrediente = new NovoIngredienteRequest("Queijo muçarela", new BigDecimal("2.0"), 200);
+        MockHttpServletRequestBuilder requestIgrediente = post("/api/ingredientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(bodyIngrediente));
+
+        mvc.perform(requestIgrediente);
+
+        List<Long> ingredientes = new ArrayList<>();
+        ingredientes.add(1L);
+        NovaPizzaRequest bodyPizza = new NovaPizzaRequest("Alho", ingredientes);
+        MockHttpServletRequestBuilder requestPizza = post("/api/pizzas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(bodyPizza));
+
+        mvc.perform(requestPizza)
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"))
+                .andExpect(redirectedUrlPattern("/api/pizzas/*"));
+
+    }
+
+
+}
