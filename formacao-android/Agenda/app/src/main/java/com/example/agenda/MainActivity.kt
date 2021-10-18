c341586156ff92f7196044e7ae491985a86b790c
João Paulo Oliveira de Lira <joaopaulooliveiradelira@MacBook-Pro-de-adesktop.local>
2021-10-18 15:12:52 -0300

    Adicionando incone ADD e incluindo AppBar com AppCompatActivity

--- formacao-android/Agenda/app/src/main/java/com/example/agenda/MainActivity.kt
+++ formacao-android/Agenda/app/src/main/java/com/example/agenda/MainActivity.kt
@@ -1,30 +1,38 @@
 package com.example.agenda
 
-import android.app.Activity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        println("ON_CREATE")
+        title = "Students"
         setContentView(R.layout.activity_main)
 
-        val listStudents = listOf("Pedro", "Thiago", "Maria", "João")
+        println("ON_CREATE")
 
         /**
          * O this é o contexto da aplicação e neste caso é o lugar onde será exibida listViewStudents.
          * Já o layout simples_list_item_1 é a view que será inclusa na listViewStudents. Cada students será posta nesta view.
          * A listStudents é a lista que alimentará a listViewStudents
          * */
+        val listStudents = listOf("Pedro", "Thiago", "Maria", "João")
         findViewById<ListView>(R.id.activity_main_list_student).let { listViewStudents ->
             listViewStudents.adapter = ArrayAdapter(
                 this,
                 android.R.layout.simple_list_item_1,
                 listStudents
             )
+        }
+
+        findViewById<FloatingActionButton>(R.id.activity_main_fab_new_student).let { fabNewStudent ->
+            fabNewStudent.setOnClickListener {
+                println("Cliquei no float button")
+            }
         }
     }
 
