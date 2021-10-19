e9761d8df7a00e43c2a3d072c0e742ba3e3f1b79
João Paulo Oliveira de Lira <joaopaulooliveiradelira@MacBook-Pro-de-adesktop.local>
2021-10-18 22:22:31 -0300

    Criando Aluno

---
+++ formacao-android/Agenda/app/src/main/java/com/example/agenda/model/Aluno.kt
@@ -0,0 +1,9 @@
+package com.example.agenda.model
+
+data class Aluno(
+    val name: String,
+    val phone: String,
+    val email: String
+    ) {
+
+}
--- formacao-android/Agenda/app/src/main/java/com/example/agenda/ui/activity/FormStudentActivity.kt
+++ formacao-android/Agenda/app/src/main/java/com/example/agenda/ui/activity/FormStudentActivity.kt
@@ -2,12 +2,32 @@ package com.example.agenda.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.example.agenda.R
+import com.example.agenda.model.Aluno
 
 class FormStudentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        title = "Form Student"
+        title = "New student"
         setContentView(R.layout.activity_form_student)
+
+        val name = findViewById<EditText>(R.id.activity_form_students_name)
+        val phone = findViewById<EditText>(R.id.activity_form_students_phone)
+        val email = findViewById<EditText>(R.id.activity_form_students_email)
+        val btnSaveStudent = findViewById<Button>(R.id.activity_form_students_btn_save)
+
+        btnSaveStudent.setOnClickListener {
+                val newStudent = Aluno(
+                    name = name.text.toString(),
+                    phone = phone.text.toString(),
+                    email = email.text.toString()
+                )
+
+                println(newStudent)
+                Toast.makeText(this, "Aluno novo cadastrado", Toast.LENGTH_SHORT).show()
+        }
     }
 }
