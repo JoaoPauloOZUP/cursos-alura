3b9be3801207f6a916123ca8c0f1cad68be11557
João Paulo Oliveira de Lira <joaopaulooliveiradelira@MacBook-Pro-de-adesktop.local>
2021-10-29 09:17:49 -0300

    Bind de summary purchase

--- formacao-android/Trips/app/src/main/java/com/zupacademy/trips/ui/activity/PurchaseSummaryActivity.kt
+++ formacao-android/Trips/app/src/main/java/com/zupacademy/trips/ui/activity/PurchaseSummaryActivity.kt
@@ -2,7 +2,13 @@ package com.zupacademy.trips.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import com.zupacademy.trips.R
+import com.zupacademy.trips.model.TravelPackage
+import com.zupacademy.trips.util.formatPriceOnView
+import com.zupacademy.trips.util.getDrawableImage
+import java.math.BigDecimal
 
 class PurchaseSummaryActivity : AppCompatActivity() {
 
@@ -10,8 +16,36 @@ class PurchaseSummaryActivity : AppCompatActivity() {
         private const val TITLE_APPBAR = "Purchase summary"
     }
 
+    private lateinit var imageLocalView: ImageView
+    private lateinit var localNameView: TextView
+    private lateinit var stayView: TextView
+    private lateinit var currentPriceView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_purchase_summary)
+        initializeAttributes()
+        dataBindOnView()
     }
+
+    private fun dataBindOnView() {
+        val travelPackage = TravelPackage(
+            local = "Sao Paulo",
+            image = "sao_paulo_sp",
+            2,
+            BigDecimal(243.99)
+        )
+
+        imageLocalView.setImageDrawable(getDrawableImage(this, travelPackage))
+        localNameView.text = travelPackage.local
+        currentPriceView.text = formatPriceOnView(travelPackage.price)
+    }
+
+    private fun initializeAttributes() {
+        imageLocalView = findViewById(R.id.purchase_localimage)
+        localNameView = findViewById(R.id.purchase_localname)
+        stayView = findViewById(R.id.purchase_stay)
+        currentPriceView = findViewById(R.id.purchase_current_price)
+    }
+
 }
