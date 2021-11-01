039cb6476175e91423ac34a33710f59c79a5771a
João Paulo Oliveira de Lira <joaopaulooliveiradelira@MacBook-Pro-de-adesktop.local>
2021-11-01 13:37:02 -0300

    Mudando a estrutura para recyclerview

--- formacao-android/Trips/app/src/main/java/com/zupacademy/trips/ui/activity/PackageListActivity.kt
+++ formacao-android/Trips/app/src/main/java/com/zupacademy/trips/ui/activity/PackageListActivity.kt
@@ -5,13 +5,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import com.zupacademy.trips.R
 import com.zupacademy.trips.model.TravelPackage
 import com.zupacademy.trips.ui.activity.adapter.PackageListAdapter
 
 class PackageListActivity : AppCompatActivity() {
 
-    private lateinit var listView: ListView
+    private lateinit var recycleview: RecyclerView
     private lateinit var dao: TravelPackageDAO
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,16 +28,16 @@ class PackageListActivity : AppCompatActivity() {
     }
 
     private fun initializeAttributes() {
-        listView = findViewById(R.id.list_travels_package)
+        recycleview = findViewById(R.id.recyclelist_travels_package)
         dao = TravelPackageDAO()
     }
 
     private fun configureAdapter() {
-        listView.adapter = PackageListAdapter(this, dao.allTravelPackage().toMutableList())
+        recycleview.adapter = PackageListAdapter(this, dao.allTravelPackage().toMutableList())
     }
 
     private fun configureOnItemClickListener() {
-        listView.setOnItemClickListener { parent, view, position, id ->
+        recycleview.setOnItemClickListener { parent, view, position, id ->
             val travelPackageClicked = dao.getPackage(position)
             startActivity(
                 Intent(this, TravelPackageSummary::class.java)
