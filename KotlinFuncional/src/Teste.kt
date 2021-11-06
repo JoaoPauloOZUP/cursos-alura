a3a213b6921aaa06458cbe6d5efb4d02ee5e16fe
João Paulo Oliveira de Lira <joaopaulooliveiradelira@MacBook-Pro-de-adesktop.local>
2021-11-06 01:11:09 -0300

    Funcao com parametros e retorno

--- KotlinFuncional/src/Teste.kt
+++ KotlinFuncional/src/Teste.kt
@@ -16,4 +16,24 @@ fun main() {
         println(a+b)
     }
     testOne(10, 10)
+
+    /**
+     * Funcao com parametros e retorno
+     * */
+    val myFunctionWithParametes: (a: Int, b: Int) -> Unit = { a: Int, b: Int ->
+        println(a+b)
+    }
+
+    val myFunctionWithReturn: (a: Int, b: Int) -> Int = ::sum
+    val myFunctionWithReturnTwo: (a: Int, b: Int) -> Int = Sum()
+    println(myFunctionWithReturn(1,1))
+    println(myFunctionWithReturnTwo(1,1))
+}
+
+/**
+ * Funcao com parametros e retorno
+ * */
+fun sum(a:Int, b:Int): Int = a+b
+class Sum : (Int, Int) -> Int {
+    override fun invoke(a: Int, b: Int): Int = a+b
 }
