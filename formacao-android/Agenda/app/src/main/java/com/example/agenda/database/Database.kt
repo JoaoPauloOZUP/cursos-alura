e5889ed7c0e96f43bb53f4c5aef8291a963136c3
João Paulo Oliveira de Lira <joaopaulooliveiradelira@MacBook-Pro-de-adesktop.local>
2021-11-10 11:06:14 -0300

    Exlcuindo e subindo um novo banco de dados

--- formacao-android/Agenda/app/src/main/java/com/example/agenda/database/Database.kt
+++ formacao-android/Agenda/app/src/main/java/com/example/agenda/database/Database.kt
@@ -13,7 +13,7 @@ import com.example.agenda.model.Student
 
 @Database(
     entities = [Student::class],
-    version = 4,
+    version = 1,
     exportSchema = false
 )
 
@@ -31,42 +31,6 @@ abstract class Database : RoomDatabase() {
                 DATABASE
             )
                 .allowMainThreadQueries()
-                .addMigrations(
-                    object : Migration(1, 2) {
-                        override fun migrate(database: SupportSQLiteDatabase) {
-                            database.execSQL("ALTER TABLE Student ADD COLUMN lastname TEXT NOT NULL DEFAULT 0")
-                        }
-                    },
-                    // Migrando para versao anterior do banco depois de uma alteracao
-                    object : Migration(2, 3) {
-                        override fun migrate(database: SupportSQLiteDatabase) {
-                            // Criar nova tabela com as informacoes desejadas
-                            database.execSQL(
-                                "CREATE TABLE IF NOT EXISTS Student_new (name TEXT NOT NULL, phone TEXT NOT NULL, email TEXT NOT NULL, id INTEGER PRIMARY KEY AUTOINCREMENT)"
-                            )
-
-                            // Copiar dados da tabela antiga para a nova
-                            database.execSQL(
-                                "INSERT INTO Student_new (name, phone, email, id) SELECT name, phone, email, id FROM Student"
-                            )
-
-                            // Remover tabela antiga
-                            database.execSQL(
-                                "DROP TABLE Student"
-                            )
-
-                            // Renomear a tabela nova com nome da tabela antiga
-                            database.execSQL(
-                                "ALTER TABLE Student_new RENAME TO Student"
-                            )
-                        }
-                    },
-                    object : Migration(3, 4) {
-                        override fun migrate(database: SupportSQLiteDatabase) {
-                            database.execSQL("ALTER TABLE Student ADD COLUMN createdAt INTEGER")
-                        }
-                    }
-                )
                 .build()
         }
     }
