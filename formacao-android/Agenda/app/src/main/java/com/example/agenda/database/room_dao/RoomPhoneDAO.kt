2e33b3ac364aa87d4dab61339b431ae155684450
João Paulo Oliveira de Lira <joaopaulooliveiradelira@MacBook-Pro-de-adesktop.local>
2021-11-12 10:25:11 -0300

    Implementando funcao de inserir e atualizar

--- formacao-android/Agenda/app/src/main/java/com/example/agenda/database/room_dao/RoomPhoneDAO.kt
+++ formacao-android/Agenda/app/src/main/java/com/example/agenda/database/room_dao/RoomPhoneDAO.kt
@@ -1,13 +1,20 @@
 package com.example.agenda.database.room_dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.OnConflictStrategy.*
 import androidx.room.Query
 import com.example.agenda.model.Phone
 
 @Dao
 interface RoomPhoneDAO {
 
+    @Insert
     fun save(vararg phone: Phone)
+
+    @Insert(onConflict = REPLACE)
+    fun update(vararg phone: Phone)
 
     @Query(
         "SELECT p.* " +
