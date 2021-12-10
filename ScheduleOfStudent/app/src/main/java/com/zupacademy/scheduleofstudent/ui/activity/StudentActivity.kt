7f46583acbe1b1821b2dda4a50cca58e6f8a73b7
João Paulo Oliveira de Lira <joaopaulooliveiradelira@MacBook-Pro-de-adesktop.local>
2021-12-10 13:35:22 -0300

    Listener na navegacao entre fragmentos

--- ScheduleOfStudent/app/src/main/java/com/zupacademy/scheduleofstudent/ui/activity/StudentActivity.kt
+++ ScheduleOfStudent/app/src/main/java/com/zupacademy/scheduleofstudent/ui/activity/StudentActivity.kt
@@ -2,17 +2,25 @@ package com.zupacademy.scheduleofstudent.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.zupacademy.scheduleofstudent.R
 
 class StudentActivity : AppCompatActivity() {
 
-    companion object {
-        private const val APPBAR_TITLE = "Student list"
+    private val navigationController: NavController by lazy {
+        findNavController(R.id.activity_student_list)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student)
-        title = APPBAR_TITLE
+        navigationController.addOnDestinationChangedListener { controller, destination, arguments ->
+            title = destination.label
+            supportActionBar?.show()
+            if(destination.id == R.id.StudentLogin) {
+                supportActionBar?.hide()
+            }
+        }
     }
 }
